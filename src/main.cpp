d8920ce067e564b462a1d081fb30103b4f60fbcf
Ovidiu Vacaru <ovidiu.vacaru19@gmail.com>
2025-09-28 17:00:53 +0100

    added ECS system basic

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,23 @@
 #include <iostream>
-#include <cstdio>
-#include <vector>
-#include "proto/protocol.hpp"
-#include "framer.hpp"
-#include <cassert>
-#include <iostream>
-
-using namespace simverse::swp;
+#include "world.hpp"
+#include "types.hpp"
 
 
-static std::vector<std::vector<uint8_t>> g_outbox;
+int main() {
+    World world;
 
-static void handle_frame(const Frame& f){
+    Entity player = world.create_entity();
+    Entity rock = world.create_entity();
 
-    switch(f.id){
-        case MsgId::CLIENT_HELLO: {
-            g_outbox.push_back(pack(MsgId::SERVER_HELLO));
-            break;
-        }
-        case MsgId::PING: {
-            g_outbox.push_back(pack(MsgId::PING, f.payload));
-            break;
-        }
-        default:
-            std::cout << "No MATCHING MESSAGE ID " << static_cast<int>(f.id) << std::endl;
-            break;
-    }
+    world.add_component(player, Position{2.5f, 3.5f});
+    world.add_component(player, Health{});
+    world.add_component(rock, Position{1.1f, 2.2f});
+
+
+    auto& player_location = world.get_component<Position>(player);
+
+    std::cout << player_location.x << player_location.y;
+
+
+
 }
-
-int main(){
-
-    
-    const auto f1 = pack(MsgId::CLIENT_HELLO);
-    const std::uint8_t data[] = {0xAA, 0xBB, 0xCC};
-    const auto f2  = pack(MsgId::PING, std::span<const std::uint8_t>(data, 3));
-
-
-    // TCP 
-
-    std::vector<std::uint8_t> blob;
-    blob.insert(blob.end(), f1.begin(), f1.end());
-    blob.insert(blob.end(), f2.begin(), f2.end());
-
-    Framer fr;
-
-
-
-    const std::size_t half = blob.size() / 2;
-    fr.feed({blob.data(), half});
-
-
-    {
-        Frame out{};
-        while (true){
-            auto s = fr.next(out);
-            if ( s == Framer::Status::Ok) { handle_frame(out); continue;}
-            if (s == Framer::Status::NeedMore) {std::cout << "Need more"; break; }
-            else {std::cout <<("error parsing frame"); break;}
-        }
-    }
-
-    // feeding the rest 
-
-    fr.feed({blob.data() + half, blob.size() - half});
-
-    {
-        Frame out{};
-        while (true){
-            auto s = fr.next(out);
-            if ( s == Framer::Status::Ok) { handle_frame(out); continue;}
-            if (s == Framer::Status::NeedMore){ 
-              std::cout << "Need more";
-              break;
-            }
-            else { 
-                std::cout <<("error parsing frame"); 
-                break;
-            }
-    }
-
-    for (const auto& buf : g_outbox) {
-        const unsigned len = (unsigned(buf[0]) << 8) | unsigned(buf[1]);
-        std::printf("OUT: bytes=%zu len=%u id=0x%02X\n", buf.size(), len, buf[2]);
-    }
-}
-}
-   
---
+++ src/world.cpp
@@ -0,0 +1,16 @@
+#include "world.hpp"
+#include "types.hpp"
+
+Entity World::create_entity(){
+    return m_next_entity_id++;
+}
+
+
+
+
+
+
+
+
+
+
