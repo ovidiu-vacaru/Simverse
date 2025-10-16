2e684ede1187effadc928fef485c88cff6890a66
Ovidiu Vacaru <ovidiu.vacaru19@gmail.com>
2025-10-16 17:54:25 +0100

    added component manager

--- src/ecs/ComponentManager.h
+++ src/ecs/ComponentManager.h
@@ -1,8 +1,22 @@
+#pragma once
+
 #include "Types.h"
 #include "Component.h"
 #include <memory>
-#include <unordered_map>
+#include <vector>
 #include <cassert>
+
+// This helper class is a private implementation detail of the ComponentManager.
+class ComponentIDGenerator {
+private:
+    static inline ComponentID s_next_component_id = 0;
+public:
+    template<typename T>
+    static ComponentID GetTypeID(){
+        static ComponentID s_type_id = s_next_component_id++;
+        return s_type_id;
+    }
+};
 
 
 template <std::size_t MaxEntites, std::size_t MaxComponents>
@@ -11,53 +25,68 @@ public:
 
     template <typename T>
     void RegisterComponent() {
+        ComponentID id = ComponentIDGenerator::GetTypeID<T>();
 
-    }
+        if(m_components_arrays.size() <= id) {
+            m_components_arrays.resize(id + 1);
+        }
 
-
-    template <typename T>
-    ComponentID GetComponentID() {
-        return getComponentID<T>();
-    }
-
-    template <typename T>
-    void AddComponent(){
-
+        std::shared_ptr<IComponentArray> component = std::make_shared<ComponentArray<T, MaxEntites>>();
+        
+        m_components_arrays[id] = component;
     }
 
     template <typename T>
-    void RemoveComponent(){
-
+    void AddComponent(Entity entity, T component){
+        GetComponentArray<T>()->InsertData(entity, component);
     }
 
-    template <typename T> 
-    T& GetComponent(){
-
+    template <typename T>
+    void RemoveComponent(Entity entity) {
+        GetComponentArray<T>()->RemoveData(entity);
     }
 
-    void EntityDestroyed(){
-
+    template <typename T>
+    T& GetComponent(Entity entity) {
+        return GetComponentArray<T>()->GetData(entity);
     }
+
+    // This function is called by the Registry whenever an entity is destroyed.
+    // Its job is to notify every single ComponentArray so they can remove any
+    // data associated with that entity, preventing orphaned components.
+    void EntityDestroyed(Entity entity) {
+        // We iterate through our vector of component storage arrays.
+        // Each element is a std::shared_ptr<IComponentArray>.
+        for (auto const& componentArray : m_components_arrays) {
+            // It's possible to have gaps in our vector if component IDs are not
+            // registered sequentially, which would leave nullptrs in the vector.
+            // This is a crucial safety check to prevent a crash from trying to
+            // access a member of a null pointer.
+            if (componentArray != nullptr) {
+                // This is a polymorphic call.
+                // We are calling the virtual EntityDestroyed() method on the base class
+                // pointer (IComponentArray). C++'s v-table mechanism ensures that the
+                // correct, specific implementation (e.g., in ComponentArray<Position>)
+                // is executed for each different component type.
+                componentArray->EntityDestroyed(entity);
+            }
+        }
+    }
+
+
 private:
 
     template <typename T>
-    std::shared_ptr<ComponentArray<T, MaxEntities>> GetComponentArray(){
-        ComponentID componentID = getComponentID<T>();
-        assert(m_component_arrays.find(componentID) != m_component_arrays.end() && "Component not registered before use")
+    std::shared_ptr<ComponentArray<T, MaxEntites>> GetComponentArray(){
+        ComponentID id = ComponentIDGenerator::GetTypeID<T>();
+        
+        assert(id < m_components_arrays.size() && "Component not registered before use.");
 
-        return std::static_pointer_cast
+        std::shared_ptr<IComponentArray> genericPtr = m_components_arrays[id];
+        
+        return std::static_pointer_cast<ComponentArray<T, MaxEntites>>(genericPtr);
     }
 
-    template <typename T>
-    static ComponentID GetComponentID(){
-        static ComponentID id = m_next_component_id++;
-        assert(id < MaxComponents && "Exceeded the maximum number of registered components.");
-        return id;
-    }
-
-    std::unordered_map<ComponentID, std::shared_ptr<IComponentArray>> m_component_arrays{};
-
-    inline static ComponentID m_next_component_id{};
-
+    std::vector<std::shared_ptr<IComponentArray>> m_components_arrays;
 };
 
